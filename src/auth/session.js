753df2d173da3a4edfb2867f7e4f4819dc32534e
luciyer <luciyer@gmail.com>
2021-02-02 11:38:03 -0300

    update app name

--- src/auth/session.js
+++ src/auth/session.js
@@ -7,7 +7,7 @@ let RedisStore = require("connect-redis")(session)
 let redisClient = redis.createClient(process.env.REDIS_URL)
 
 const sessionOptions = {
-  name: "lowtide.auth",
+  name: "Lowtide",
   secret: process.env.SESSION_SECRET,
   cookie: {
     maxAge: (60 * 60000),
